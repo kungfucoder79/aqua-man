c8e20f84661edf1a5efc37c28d693a470fdead64
Tim Bush <tbush79@gmail.com>
2018-09-06 09:58:01 -0400

    added validation to TankSpecs view and model

--- AquaMan/Models/TankSpecs.cs
+++ AquaMan/Models/TankSpecs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,16 +14,22 @@ namespace AquaMan.Models
         /// <summary>
         /// Height of the tank
         /// </summary>
+        [Required]
+        [RegularExpression(@"\d+(\.\d{1,2})?$", ErrorMessage = "* Must be a two digit decimal (1.00)")]
         public double Height { get; set; }
 
         /// <summary>
         /// Width of the tank
         /// </summary>
+        [Required]
+        [RegularExpression(@"\d+(\.\d{1,2})?$", ErrorMessage = "* Must be a two digit decimal (1.00)")]
         public double Width { get; set; }
 
         /// <summary>
         /// Depth of the tank
         /// </summary>
+        [Required]
+        [RegularExpression(@"\d+(\.\d{1,2})?$", ErrorMessage = "* Must be a two digit decimal (1.00)")]
         public double Depth { get; set; }
     }
 }
