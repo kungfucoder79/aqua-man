9caa5d3af0a60fcd58c0985cdedab92f3f3b671c
Tim Bush <tbush79@gmail.com>
2018-10-24 23:43:12 -0400

    removed unneeded properties from the progress bar tag helper

--- AquaMan/Helpers/ProgressTagHelper.cs
+++ AquaMan/Helpers/ProgressTagHelper.cs
@@ -9,14 +9,11 @@ namespace AquaMan.Helpers
     [HtmlTargetElement("progressbar")]
     public class ProgressTagHelper : TagHelper
     {
-        public double WaterHeight { get; set; }
-
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "div";
             output.TagMode = TagMode.StartTagAndEndTag;
             output.Attributes.SetAttribute("class", "progress-bar progress-bar-success progress-bar-striped");
-            output.Attributes.SetAttribute("style", "width: " + WaterHeight + "%");
         }
     }
 
