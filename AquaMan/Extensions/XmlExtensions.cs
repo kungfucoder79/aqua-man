b147417da79ac72b919b15ffedbccfdcbf1a069f
Tim Bush <tbush79@gmail.com>
2018-10-25 23:56:33 -0400

    Moved the XmlExtensions to the AquaMan project.

--- AquaMan/Extensions/XmlExtensions.cs
+++ AquaMan/Extensions/XmlExtensions.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Text;
 using System.Xml;
 
-namespace Aqua_Control
+namespace AquaMan.Extensions
 {
     public static class XmlExtensions
     {
--- AquaMan/Services/FormDataService.cs
+++ AquaMan/Services/FormDataService.cs
@@ -1,4 +1,5 @@
 ﻿using Aqua_Control;
+using AquaMan.Extensions;
 using AquaMan.Models;
 using System;
 using System.Collections.Generic;
